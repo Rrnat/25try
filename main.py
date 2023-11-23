0ca25e4f753f66746970ee7e978ac09332fa76c2
Mish56372 <151863134+Mish56372@users.noreply.github.com>
2023-11-23 19:45:49 +0300

    new_main.py_1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+name = "Michael"
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     print_hi('PyCharm-dfkckfdjjdfgnadfkbngjfgdf')
     print_hi('PyCharm-dfkckfdjjdfgnadfkbngjfgdf')
     print_hi('PyCharm-dfkckfdjjdfgnadfkbngjfgdf')
+    print_hi()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
